b475b0d345d3b7b0ec54a7ed06ce9579d65e18a6
Amisi Deffy <amisifikirini@gmail.com>
2024-02-25 15:57:04 +0100

    deploy conf

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -20,4 +20,70 @@ module.exports = {
     //   ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
     // },
   },
+  // development: {
+  //   host: process.env.DEV_DB_HOST,
+  //   port: process.env.DEV_DB_PORT,
+  //   dialect: process.env.DEV_DB_DIALECT,
+  //   dialectModule: require("pg"),
+  //   username: process.env.DEV_DB_USER,
+  //   password: process.env.DEV_DB_PASSWORD,
+  //   database: process.env.DEV_DB_DATABASE_NAME,
+  //   define: {
+  //     charset: "utf8",
+  //     collate: "utf8_general_ci",
+  //     timestamp: false,
+  //     underscored: true,
+  //   },
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //     // it must be decomment for production's mode
+  //     // ssl: {
+  //     //   ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
+  //     // },
+  //   },
+  // },
+  // production:{
+  //   host: process.env.PROD_DEV_DB_HOST,
+  //   port: process.env.PROD_DB_PORT,
+  //   dialect: process.env.PROD_DEV_DB_DIALECT,
+  //   dialectModule: require('pg'),
+  //   username: process.env.PROD_DEV_DB_USER,
+  //   password: process.env.PROD_DEV_DB_PASSWORD,
+  //   database: process.env.PROD_DEV_DB_DATABASE_NAME,
+  //   define: {
+  //     charset: "utf8",
+  //     collate: "utf8_general_ci",
+  //     timestamp: false,
+  //     underscored: true,
+  //   },
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //     // it must be decomment for production's mode
+  //     // ssl: {
+  //     //   ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
+  //     // },
+  //   },
+  // },
+  // test:{
+  //   host: process.env.TEST_DEV_DB_HOST,
+  //   port: process.env.TEST_DB_PORT,
+  //   dialect: process.env.TEST_DEV_DB_DIALECT,
+  //   dialectModule: require('pg'),
+  //   username: process.env.TEST_DEV_DB_USER,
+  //   password: process.env.TEST_DEV_DB_PASSWORD,
+  //   database: process.env.TEST_DEV_DB_DATABASE_NAME,
+  //   define: {
+  //     charset: "utf8",
+  //     collate: "utf8_general_ci",
+  //     timestamp: false,
+  //     underscored: true,
+  //   },
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //     // it must be decomment for production's mode
+  //     // ssl: {
+  //     //   ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
+  //     // },
+  //   },
+  // }
 };
